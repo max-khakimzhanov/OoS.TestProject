9756d34e16c8bac1681fd758289843da52d18d84
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2025-03-06 07:16:50 +0200

    Add project files.

---
+++ OoS.TestProject.DAL/Entities/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OoS.TestProject.DAL.Entities
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public int TeacherId { get; set; }
+        public int Credits { get; set; }
+        public Teacher Teacher { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
---
+++ OoS.TestProject.DAL/Entities/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OoS.TestProject.DAL.Entities
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+}

416c81a41774ecd7d3010cd52c916c8263d9f481
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2025-03-09 23:37:56 +0200

    Add CourseController with CRUD endpoints

---
+++ OoS.TestProject.WebApi/Controllers/CourseController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OoS.TestProject.BLL.Dto.Course;
+using OoS.TestProject.BLL.MediatR.CourseEntity.Create;
+using OoS.TestProject.BLL.MediatR.CourseEntity.Delete;
+using OoS.TestProject.BLL.MediatR.CourseEntity.GetAll;
+using OoS.TestProject.BLL.MediatR.CourseEntity.GetById;
+using OoS.TestProject.BLL.MediatR.CourseEntity.Update;
+
+namespace OoS.TestProject.WebApi.Controllers
+{
+    public class CourseController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return HandleResult(await Mediator.Send(new GetAllCourseQuery()));
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            return HandleResult(await Mediator.Send(new GetByIdCourseQuery(id)));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateCourseDto course)
+        {
+            return HandleResult(await Mediator.Send(new CreateCourseCommand(course)));
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCourseDto course)
+        {
+            return HandleResult(await Mediator.Send(new UpdateCourseCommand(id, course)));
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            return HandleResult(await Mediator.Send(new DeleteCourseCommand(id)));
+        }
+    }
+}

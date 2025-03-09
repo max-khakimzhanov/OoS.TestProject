f5eac1437ff2c374d687f54d09d839b3b1d4d1b1
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2025-03-09 23:38:45 +0200

    Add StudentController with CRUD endpoints

---
+++ OoS.TestProject.WebApi/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OoS.TestProject.BLL.Dto.Student;
+using OoS.TestProject.BLL.MediatR.Student.Create;
+using OoS.TestProject.BLL.MediatR.Student.Delete;
+using OoS.TestProject.BLL.MediatR.Student.GetAll;
+using OoS.TestProject.BLL.MediatR.Student.GetById;
+using OoS.TestProject.BLL.MediatR.Student.Update;
+
+namespace OoS.TestProject.WebApi.Controllers
+{
+    public class StudentController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return HandleResult(await Mediator.Send(new GetAllStudentQuery()));
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            return HandleResult(await Mediator.Send(new GetByIdStudentQuery(id)));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateStudentDto student)
+        {
+            return HandleResult(await Mediator.Send(new CreateStudentCommand(student)));
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStudentDto student)
+        {
+            return HandleResult(await Mediator.Send(new UpdateStudentCommand(id, student)));
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            return HandleResult(await Mediator.Send(new DeleteStudentCommand(id)));
+        }
+    }
+}

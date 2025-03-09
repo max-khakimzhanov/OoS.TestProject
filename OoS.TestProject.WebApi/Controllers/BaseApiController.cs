ad8825e6e900a4264da5c19d8a8b5772dc95e05d
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2025-03-09 23:37:20 +0200

    Add BaseApiController with Mediator and result handling

---
+++ OoS.TestProject.WebApi/Controllers/BaseApiController.cs
@@ -0,0 +1,31 @@
+﻿using FluentResults;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OoS.TestProject.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class BaseApiController : ControllerBase
+    {
+        private IMediator? _mediator;
+
+        protected IMediator Mediator => _mediator ??=
+            HttpContext.RequestServices.GetService<IMediator>()!;
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result.IsSuccess)
+            {
+                if (result.Value == null)
+                {
+                    return NotFound("Found result matching null");
+                }
+
+                return Ok(result.Value);
+            }
+
+            return BadRequest(result.Reasons);
+        }
+    }
+}

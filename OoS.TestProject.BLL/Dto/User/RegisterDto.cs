1cc55f8138b70096f427e537ef65be9cb3cefae3
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2025-03-11 03:51:06 +0200

    Add LoginDto and RegisterDto classes for user management

---
+++ OoS.TestProject.BLL/Dto/User/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace OoS.TestProject.BLL.Dto.User
+{
+    public class RegisterDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+    }
+}

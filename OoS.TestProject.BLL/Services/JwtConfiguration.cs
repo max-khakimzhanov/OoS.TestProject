6ca914db9fdd93bfafea2e1f7bf4d44f360af126
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2025-03-11 04:01:35 +0200

    Add JwtConfiguration class for JWT settings
    A new class `JwtConfiguration` has been added to the `OoS.TestProject.BLL.Services` namespace. This class includes properties for configuring JWT (JSON Web Token) settings, such as the issuer, audience, secret key, and lifetimes for access and refresh tokens.

---
+++ OoS.TestProject.BLL/Services/JwtConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace OoS.TestProject.BLL.Services
+{
+    public class JwtConfiguration
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string SecretKey { get; set; }
+        public int AccessTokenLifetime { get; set; }
+        public int RefreshTokenLifetime { get; set; }
+    }
+}

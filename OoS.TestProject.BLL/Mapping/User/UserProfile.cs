dda15588a4b995dcecf7c43881704478648ceb1a
Maksym Khakimzhanov <max.hakimzhanov@gmail.com>
2025-03-11 03:55:05 +0200

    Add UserProfile for mapping RegisterDto to UserEntity

---
+++ OoS.TestProject.BLL/Mapping/User/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using OoS.TestProject.BLL.Dto.User;
+using UserEntity = OoS.TestProject.DAL.Entities.User;
+
+namespace OoS.TestProject.BLL.Mapping.User
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile() 
+        {
+            CreateMap<RegisterDto, UserEntity>();
+        }
+    }
+}
